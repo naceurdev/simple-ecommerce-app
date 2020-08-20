e4d1823acba89348861bc4c4d24674bc372444ba
Naceur <naceur.benyahia@oyez.fr>
2020-08-20 15:07:58 +0200

    remove unnecessary path

--- Documents/Oyez/ecommerce-app/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import productReducer from './productReducer';
-import cartReducer from './cartReducers';
-
-export default combineReducers({
-    products: productReducer,
-    cart: cartReducer,
-})
